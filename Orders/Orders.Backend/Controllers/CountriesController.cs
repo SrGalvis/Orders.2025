1f6e76bff822db045cfe3064a7b6fdc6e5d87f3d
Leidy Galvis <carlosgalvis@gmail.com>
2025-08-22 13:15:27 -0500

    Add PUT and DELETE for countries

--- Orders/Orders.Backend/Controllers/CountriesController.cs
+++ Orders/Orders.Backend/Controllers/CountriesController.cs
@@ -22,10 +22,42 @@ public class CountriesController : ControllerBase
         return Ok(await _context.Countries.ToListAsync());
     }
 
+    [HttpGet("{id}")]
+    public async Task<IActionResult> GetAsync(int id)
+    {
+        var country = await _context.Countries.FirstOrDefaultAsync(x => x.Id == id);
+        if (country == null)
+        {
+            return NotFound();
+        }
+        return Ok(country);
+    }
+
     [HttpPost]
     public async Task<IActionResult> PostAsync(Country country)
     {
         _context.Countries.Add(country);
+        await _context.SaveChangesAsync();
+        return Ok(country);
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> DeleteAsync(int id)
+    {
+        var country = await _context.Countries.FirstOrDefaultAsync(x => x.Id == id);
+        if (country == null)
+        {
+            return NotFound();
+        }
+        _context.Countries.Remove(country);
+        await _context.SaveChangesAsync();
+        return Ok(country);
+    }
+
+    [HttpPut]
+    public async Task<IActionResult> PutAsync(Country country)
+    {
+        _context.Update(country);
         await _context.SaveChangesAsync();
         return Ok(country);
     }
