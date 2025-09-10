defd36ff08f16bc1f1379499bc01cc89a8356111
Leidy Galvis <carlosgalvis@gmail.com>
2025-09-10 14:07:36 -0500

    Add Entities, migration Citys and States

--- Orders/Ordes.Shared/Entities/Country.cs
+++ Orders/Ordes.Shared/Entities/Country.cs
@@ -1,8 +1,10 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using Orders.Shared.Entities;
+using Orders.Shared.Interfaces;
+using System.ComponentModel.DataAnnotations;
 
 namespace Ordes.Shared.Entities;
 
-public class Country
+public class Country : IEntityWithName
 {
     public int Id { get; set; }
 
@@ -10,4 +12,9 @@ public class Country
     [MaxLength(100, ErrorMessage = "El campo {0} no puede tener mas de {1} caracteres.")]
     [Required(ErrorMessage = "El campo {0} es obligatorio.")]
     public string Name { get; set; } = null!;
+
+    public ICollection<State>? States { get; set; }
+
+    [Display(Name = "Estados/Departamentos")]
+    public int StatesNumber => States == null || States.Count == 0 ? 0 : States.Count;
 }
